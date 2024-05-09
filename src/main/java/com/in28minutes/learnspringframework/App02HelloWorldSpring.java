ad07d8d1889f56b9d77e6264fc7b9106b3be4df2
Fahriddin <Fahriddin@WIN-U2794NDE3MO>
2024-05-09 02:47:24 -0400

    Beginning with Spring Level

--- src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -5,13 +5,15 @@ import com.in28minutes.learnspringframework.game.MarioGame;
 import com.in28minutes.learnspringframework.game.PacMan;
 import com.in28minutes.learnspringframework.game.SuperContra;
 
-public class AppGamingBasicJava {
+public class App02HelloWorldSpring {
 
 	public static void main(String[] args) {
-	    var game = new MarioGame();
+//	    var game = new MarioGame();
 //	    var game = new SuperContra();
-//	    var game = new PacMan();
-	    var gameRunner = new GameRunner(game);
+	    var game = new PacMan(); // 1: Object creation
+	    var gameRunner = new GameRunner(game);  
+	    	// 2: Object Creation + Wiring of Dependencies
+	    	// Game is a Dependency of GameRunner
 	    gameRunner.run();
 
 	}

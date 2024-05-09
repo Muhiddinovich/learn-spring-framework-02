a8e41e91b2a1be14bc0dff1d30f0f7808de14183
Fahriddin <Fahriddin@WIN-U2794NDE3MO>
2024-05-09 02:49:13 -0400

    Spring initial

--- src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -1,21 +1,11 @@
 package com.in28minutes.learnspringframework;
 
-import com.in28minutes.learnspringframework.game.GameRunner;
-import com.in28minutes.learnspringframework.game.MarioGame;
-import com.in28minutes.learnspringframework.game.PacMan;
-import com.in28minutes.learnspringframework.game.SuperContra;
-
 public class App02HelloWorldSpring {
 
 	public static void main(String[] args) {
-//	    var game = new MarioGame();
-//	    var game = new SuperContra();
-	    var game = new PacMan(); // 1: Object creation
-	    var gameRunner = new GameRunner(game);  
-	    	// 2: Object Creation + Wiring of Dependencies
-	    	// Game is a Dependency of GameRunner
-	    gameRunner.run();
-
+		//1: Launch a spring Context
+		
+		//2: Configure the things that we want Spring to manage - @Configuration
 	}
 
 }
